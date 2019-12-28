a24145290edc5a21a258202ed8f6a4452fc3b8c5
kabewall <kouhei.kabe@gmail.com>
2019-12-28 16:20:38 +0900

    関数を整理しました。

--- AcousticSignalProcessing.py
+++ AcousticSignalProcessing.py
@@ -62,8 +62,6 @@ def readWave(filename):
     Return: Numpy Array(float64) [channels, length]
     """
 
-
-
     wr = wave.open(filename, 'r')
     params = wr.getparams()
     nchannels = params[0]   # Number of Cannels
@@ -108,32 +106,30 @@ def writeWave(file_name, data, params=(1, 3, 48000)):
         data = np.reshape(data, [data.shape[0], 1])
     else:
         nchannels = data.shape[0]
-    if nchannels == params[0]:
-        data = data.T
-        audio = wave.Wave_write(file_name) # filenameというファイルに書き出し
-        # パラメータ設定
-        audio.setnchannels(params[0])
-        audio.setsampwidth(params[1])
-        audio.setframerate(params[2])
 
-        data = (data*(2**(8*params[1]-1)-1)).reshape(data.size, 1)
-        if params[1] == 1:
-            data = data + 128
-            frames = data.astype(np.uint8).tostring()
-        elif params[1] == 2:
-            frames = data.astype(np.int16).tostring()
-        elif params[1] == 3:
-            a32 = np.asarray(data, dtype = np.int32)
-            a8 = ( a32.reshape(a32.shape + (1,)) >> np.array([0, 8, 16]) ) & 255
-            frames = a8.astype(np.uint8).tostring()
-        elif params[1] == 4:
-            frames = data.astype(np.int32).tostring()
+    data = data.T
+    audio = wave.Wave_write(file_name) # filenameというファイルに書き出し
+    # パラメータ設定
+    audio.setnchannels(params[0])
+    audio.setsampwidth(params[1])
+    audio.setframerate(params[2])
 
-        audio.writeframes(frames) # 出力データ設定
-        audio.close() # ファイルを閉じる
-        return
-    else:
-        print("Input channels are wrong.")
+    data = (data*(2**(8*params[1]-1)-1)).reshape(data.size, 1)
+    if params[1] == 1:
+        data = data + 128
+        frames = data.astype(np.uint8).tostring()
+    elif params[1] == 2:
+        frames = data.astype(np.int16).tostring()
+    elif params[1] == 3:
+        a32 = np.asarray(data, dtype = np.int32)
+        a8 = ( a32.reshape(a32.shape + (1,)) >> np.array([0, 8, 16]) ) & 255
+        frames = a8.astype(np.uint8).tostring()
+    elif params[1] == 4:
+        frames = data.astype(np.int32).tostring()
+
+    audio.writeframes(frames) # 出力データ設定
+    audio.close() # ファイルを閉じる
+    return
 
 def mkTottedashi(signal, ir, savePath):
     """
@@ -148,8 +144,8 @@ def mkTottedashi(signal, ir, savePath):
 
 
     if ir.shape[0] != 24:
-        print("IRのチャンネル数が%d chです。"%(ir.shape[0]))
-        print("IRのチャンネル数を見直してください。")
+        print("the Number of IR channels is %d"%(ir.shape[0]))
+        print("Recheck the Number of Channels")
         return
 
     lenSignal = signal.shape[0]
@@ -167,6 +163,7 @@ def mkTottedashi(signal, ir, savePath):
 def fig_freqz(signal, fs=48000, title="Frequency Characteristic", label="signal", legend=False):
     """
     Plot a frequency charasteristic of a signal
+    No window processing
 
     Parameters
     ----------------------
@@ -201,6 +198,7 @@ def fig_freqz_thirdBand(signal, fs=48000, title="Band-Frequency Charasteristic",
         for j in range(bandFreqList[i], bandFreqList[i+1]+1, 1):
             bandLevel[i] += signalF[j] ** 2 / (0.23 * freq[i])
     plt.plot(freq[:31], 10*np.log10(bandLevel / np.max(bandLevel)) ,'s-', label=label)
+    # plt.bar(bandFreqList, bandLevel)
     plt.xscale('log')
     plt.xlim(18, fs//2)
     plt.xlabel("Frequency [Hz]")
@@ -241,7 +239,7 @@ def fig_all(signal, fs=48000, num=1, time_title="Signal wave", time_xaxis="time"
     plt.suptitle(suptitle)
 
 # tspからIRを作成
-def tsp2ir(itspPath, signal1Path, signal2Path, irLength=8192):
+def tsp2ir(itspPath, signalPath, irLength=8192):
     """
     convert responces of tsp signal to IR
 
@@ -255,9 +253,7 @@ def tsp2ir(itspPath, signal1Path, signal2Path, irLength=8192):
     -------------------------
     ir: Impulse Responce (numpy array)
     """
-    signal1 = readWave(signal1Path)
-    signal2 = readWave(signal2Path)
-    signal = (signal1 + signal2) / 2
+    signal = readWave(signalPath)
     itsp = readWave(itspPath)
     itspF = fft.fft(itsp)
 
@@ -322,235 +318,3 @@ def wav2npy(channel, wavPath, npyPath):
 		else:
 			source = np.concatenate((source, np.load("%s/%02d.wav"%(wavPath, i+1))), axis=1)
 	np.save("%s/sound.npy"%(npyPath))
-
-def binary2float(frames, length, sampwidth):
-    # binary -> int
-    if sampwidth == 1:
-        data = np.frombuffer(frames, dtype=np.uint8)
-        data = data - 128
-    elif sampwidth == 2:
-        data = np.frombuffer(frames, dtype=np.int16)
-    elif sampwidth == 3:
-        a8 = np.fromstring(frames, dtype=np.uint8)
-        tmp = np.empty([length, 4], dtype=np.uint8)
-        tmp[:, :sampwidth] = a8.reshape(-1, sampwidth)
-        tmp[:, sampwidth:] = (tmp[:, sampwidth - 1:sampwidth] >> 7) * 255
-        data = tmp.view('int32')[:, 0]
-    elif sampwidth == 4:
-        data = np.frombuffer(frames, dtype=np.int32)
-    # Normalize to -1.0 ≤ sample < 1.0
-    data = data.astype(float) / 2 ** (8 * sampwidth - 1)
-
-    return data
-
-def float2binary(data, sampwidth):
-    data = (data * (2 ** (8 * sampwidth - 1) - 1)).reshape(data.size, 1)
-
-    if sampwidth == 1:
-        data = data + 128
-        frames = data.astype(np.uint8).tostring()
-    elif sampwidth == 2:
-        frames = data.astype(np.int16).tostring()
-    elif sampwidth == 3:
-        a32 = np.asarray(data, dtype = np.int32)
-        a8 = (a32.reshape(a32.shape + (1,)) >> np.array([0, 8, 16])) & 255
-        frames = a8.astype(np.uint8).tostring()
-    elif sampwidth == 4:
-        frames = data.astype(np.int32).tostring()
-
-    return frames
-
-def max_24ch(data, fs=48000, cor=10):
-    if data.shape[0] == 24:
-        print("24chの音源を[24, :]のndarray配列で入力してください。")
-    else:
-        max_list = np.zeros([3, 8])
-        for i in range(24):
-            max_list[(i+1)//9 , ((i+1)%8 + 2)%8] = np.max(data[i])
-        plt.matshow(maxlist)
-
-def nextpow2(n):
-    l = np.ceil(np.log2(n))
-    m = int(np.log2(2 ** l))
-    return m
-
-def progressbar(percent, end=1, bar_length=40, slug='#', space='-', tail=''):
-    percent = percent / end # float
-    slugs = slug * int( round( percent * bar_length ) )
-    spaces = space * ( bar_length - len( slugs ) )
-    bar = slugs + spaces
-    sys.stdout.write("\r[{bar}] {percent:.1f}% {tail}".format(
-    	bar=bar, percent=percent*100., tail=tail
-    ))
-    sys.stdout.flush()
-    if percent == 1:
-        print()
-
-def matmul_ver1(A, B):
-    C = np.empty([A.shape[0], A.shape[2]], dtype=np.complex128)
-    for ch in range(A.shape[0]):
-        C[ch, :] = np.sum(A[ch, :, :] * B, axis=0)
-    return C
-
-def matmul_ver2(A, B):
-    C = np.empty([A.shape[0], A.shape[2]], dtype=np.complex128)
-    for fbin in range(A.shape[2]):
-        C[:, fbin] = np.dot(A[:, :, fbin], B[:, fbin])
-    return C
-
-def convInvFast(filename_input, filename_output, filename_filter, channels=[24, 24]):
-    nchannels_output = channels[0]
-    nchannels_input = channels[1]
-    # len_fir = 8192
-    gain_dB = -13
-
-    startRealTime = time.perf_counter()
-    startClockTime = time.process_time()
-
-    Gain = 10 ** (gain_dB / 20)
-
-    # fir 畳み込み
-    # print('Loading FIR', end='')
-    # sys.stdout.flush()
-    # cnt = 0
-    # err = 0
-    # fir = np.zeros([nchannels_output, nchannels_input, len_fir])
-    # for index_lsp in range(nchannels_output):
-    #     ch_lsp = index_lsp + 1
-    #     for index_mic in range(nchannels_input):
-    #         ch_mic = index_mic + 1
-    #
-    #         try:
-    #             filename = "%s/inv_s%02d_m%02d.npy" % (path2inv, ch_lsp, ch_mic)
-    #             fir[index_lsp, index_mic, :] = np.load(filename)
-    #         except:
-    #             err += 1
-    #         finally:
-    #             cnt += 1
-    # print(' [%d]'%(cnt - err))
-
-    fir = np.load(filename_filter)
-    # invfilter [sp, mic, len]
-    len_fir = fir.shape[2]
-
-    # 入力ファイル マルチチャンネルファイル
-    w_read = wave.open(filename_input, 'r')
-    params = w_read.getparams()
-
-    ws_input = params[1]
-    fs = params[2]
-    nframes = params[3]
-
-    # 出力ファイル マルチチャンネルファイル
-    w_write = wave.open(filename_output, 'wb')
-    w_write.setparams((24, ws_input, fs, 0, 'NONE', 'not compressed'))
-
-
-
-    # ブロック長の決定
-    M = len_fir
-    N = 2 ** nextpow2(M + M * 4 - 1)
-    L = N - M + 1
-    nblocks = int(np.ceil((nframes + M - 1) / L))
-
-    fir_f = np.fft.rfft(fir, axis=2, n=N)
-    del fir
-
-    block = np.empty([nchannels_input, N])
-    block.shape
-    block[:, :-L] = 0.
-
-    flag_continue = False
-    convStartTime = time.time()
-
-
-    for l in range(nblocks - 1):
-        progressbar(l, nblocks)
-        remain = w_read.getparams()[3] - w_read.tell()
-
-
-        if remain >= L:
-            frames = w_read.readframes(L)
-            b = binary2float(frames, L, ws_input)
-            block[:, -L:] = np.reshape(b, [b.shape[0]//24, 24]).T
-        else:
-            frames = w_read.readframes(remain)
-            b = binary2float(frames, remain, ws_input)
-            block[:, -L:-L+remain] = np.reshape(b, [b.shape[0]//24, 24]).T
-            block[:, -L+remain:] = 0.
-
-        block_f = np.fft.rfft(block, n=N)
-        out_f = matmul_ver1(fir_f, block_f)
-        out = Gain * np.fft.irfft(out_f, axis=1)[:, -L:]
-
-        if not flag_continue:
-            if np.max(np.abs(out)) > 1:
-                saturation_dB = 20 * np.log10(np.max(np.abs(out)))
-                print('Saturation detected. (%.1f dB) Continue? (y/n):'
-                      % saturation_dB, end='')
-                yesno = input()
-                if yesno == 'y':
-                    flag_continue = True
-                else:
-                    print('中断したぜ！')
-                    break
-
-        f = np.reshape(out.T, [out.size,])
-        frames = float2binary(f, ws_input)
-        w_write.writeframes(frames)
-
-        block[:, :-L] = block[:, L:]
-
-        elapsedTime = time.time() - convStartTime
-        remainTime = elapsedTime / (l + 1) * (nblocks - l - 1)
-        s_remain = '%02d:%02d:%02d' % (remainTime // 3600,
-                (remainTime % 3600) // 60, np.ceil(remainTime % 60))
-        progressbar(l + 1, nblocks, tail='Remain %s' % (s_remain))
-
-    else:
-        #progressbar(l + 1, nblocks)
-        # input
-        remain = w_read[0].getparams()[3] - w_read[0].tell()
-        if remain >= L:
-            for ch in range(nchannels_input):
-                frames = w_read[ch].readframes(L)
-                block[ch, -L:] = binary2float(frames, L, ws_input)
-        else:
-            for ch in range(nchannels_input):
-                frames = w_read[ch].readframes(remain)
-                block[ch, -L:-L + remain] = binary2float(frames, remain, ws_input)
-            block[:, -L + remain:] = 0.
-
-        # convolution & write
-        block_f = np.fft.rfft(block, n=N)
-        out_f = matmul_ver1(fir_f, block_f)
-        out = Gain * np.fft.irfft(out_f, axis=1)[:, -L:]
-
-        if not flag_continue:
-            if np.max(np.abs(out)) > 1:
-                print('Saturation detected. Continue? (y/n): ', end='')
-                yesno = input()
-                if yesno == 'y':
-                    flag_continue = True
-                else:
-                    print('中断')
-
-        # convolution & write
-        block_f = np.fft.rfft(block, n=N)
-        nDiscard = L * nblocks - (nframes + M - 1)
-        out_f = matmul_ver1(fir_f, block_f)
-        out = Gain * np.fft.irfft(out_f, axis=1)[:, -L:-nDiscard]
-        for ch in range(nchannels_output):
-            frames = float2binary(out[ch, :], ws_output)
-            w_write[ch].writeframes(frames)
-
-    progressbar(1, tail='Remain 00:00:00')
-    w_read.close()
-    w_write.close()
-
-
-
-if __name__ == "__main__":
-    fs = 48000
-    pink = mkpink(1, fs)
-    writeWave("/Users/kabe/Desktop/pink2.wav", pink)
