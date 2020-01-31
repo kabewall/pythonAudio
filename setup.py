71cd3012a6e6f42a2162f4fe993236bb8f34a7a5
kabewall <kouhei.kabe@gmail.com>
2020-01-31 10:21:33 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "pythonAudio",
+    version = '1.0.0',
+    url = 'https://github.com/kabewall/pythonAudio.git',
+    license = "MTI",
+    author = "kabewall",
+    author_email = "kouhei.kabe@gmail.com",
+    keywords = '',
+    packages = find_packages(),
+    install_requires = []
+)
