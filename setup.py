e2b68b48283b9e47d9d381355063b4451a8ac523
kabewall <kouhei.kabe@gmail.com>
2020-01-31 10:41:33 +0900

    add setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,20 @@
 
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
 setup(
     name = "pythonAudio",
-    version = '1.0.0',
+    version = '1.0.0'
     url = 'https://github.com/kabewall/pythonAudio.git',
-    license = "MTI",
+    long_description = 'readme'
+    license = license,
     author = "kabewall",
     author_email = "kouhei.kabe@gmail.com",
     keywords = '',
-    packages = find_packages(),
-    install_requires = []
+    packages = find_packages(exclude=('test', 'docs'))
 )
