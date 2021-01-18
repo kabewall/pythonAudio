d4204cd0001293086a3378d2fca8f7c8875bc276
kabewall <kouhei.kabe@gmail.com>
2021-01-18 14:53:10 +0900

    add some argumetnt

--- wavePlot.py
+++ wavePlot.py
@@ -4,18 +4,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.fftpack as fft
 
-def fig_time(signal, fs=48000, title="wave form", xaxis="time", label="signal", legend=False):
+def fig_time(signal, fs=48000, title="wave form", xaxis="time", label="signal", legend=False, color=None, ls=None):
 
     if signal.ndim != 1:
         error = "dim of signal must be 1."
         return print(error)
 
     if xaxis == "tap":
-        plt.plot(signal, label=label)
+        plt.plot(signal, label=label, color=color, ls=ls)
         plt.xlabel("tap")
     elif xaxis == "time":
         time = np.linspace(0, signal.shape[0]/fs, signal.shape[0])
-        plt.plot(time, signal, label=label)
+        plt.plot(time, signal, label=label, color=color, ls=ls)
         plt.xlabel("time [s]")
     else:
         error = "xaxis must be \"tap\" or \"time\""
@@ -27,7 +27,7 @@ def fig_time(signal, fs=48000, title="wave form", xaxis="time", label="signal", 
         legend()
 
 
-def fig_freqz(signal, fs=48000, title="Frequency Characteristic", label="signal", legend=False):
+def fig_freqz(signal, fs=48000, title="Frequency Characteristic", label="signal", legend=False, color=None, ls=None, normalize_f=None):
 
     if signal.ndim != 1:
         error = "dim of signal must be 1."
@@ -37,7 +37,12 @@ def fig_freqz(signal, fs=48000, title="Frequency Characteristic", label="signal"
     signalF = fft.fft(signal)
     N = signalF.shape[0]
     f = fft.fftfreq(signalF.shape[0], d=1/fs)
-    plt.plot(f[:N//2], 20*np.log10(np.abs(signalF[:N//2])/np.max(np.abs(signalF[:N//2]))), label=label)
+    if normalize_f == None:
+        norm_value = np.max(np.abs(signalF[:N//2]))
+    else:
+        normalize_tap = int(normalize_f * (N//2) / (fs//2))
+        norm_value = np.abs(signalF[normalize_tap])
+    plt.plot(f[:N//2], 20*np.log10(np.abs(signalF[:N//2])/norm_value), label=label, color=color, ls=ls)
     plt.xscale('log')
     plt.xlim(20, fs//2)
     plt.xlabel("Frequency [Hz]")
